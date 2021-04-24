1c6225bc1a1efc4330b7e53b3b33ce2869046778
Kurt <kurtorioque112@gmail.com>
2021-04-24 16:33:15 +0800

    security added

--- src/main/java/com/API/PurchaseOrder/configuration/SecurityWebApplicationInitializer.java
+++ src/main/java/com/API/PurchaseOrder/configuration/SecurityWebApplicationInitializer.java
@@ -1,4 +1,6 @@
 package com.API.PurchaseOrder.configuration;
 
-public class SecurityWebApplicationInitializer {
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 }
